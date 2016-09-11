2279aadbe64585ad99df24151a3fe56780bad825
Hemant <hvlk369@gmail.com>
2016-09-10 22:07:36 -0400

    Add Folder

---
+++ src/main/java/com/example/Greeting.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Greeting {
+	
+	private long id=0;
+    private String content=null;
+    
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+	public long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+}
